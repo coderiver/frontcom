113f1c51d488cc264eb9db829cc746fbf64464af
mirantel <magi.mirantel@gmail.com>
2012-12-24 18:15:59 +0200

    new block

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-
+// scrollable
+	$('#clients .slider__holder').scrollable({
+		next:'#clients .slider__next',
+		prev:'#clients .slider__prev',
+		circular:true
+	});
+	$('#progects .slider__holder').scrollable({
+		next:'#progects .slider__next',
+		prev:'#progects .slider__prev',
+		circular:true
+	});
 });
