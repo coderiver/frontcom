4d5703547e04a6ff64530b0bbc03e0b79ab5fe0f
mirantel <magi.mirantel@gmail.com>
2012-12-25 18:47:11 +0200

    add

--- js/common.js
+++ js/common.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
 // scrollable
-	$('#clients .slider__holder').scrollable({
-		next:'#clients .slider__next',
-		prev:'#clients .slider__prev',
-		circular:true
-	});
+	//$('#clients .slider__holder').scrollable({
+	//	next:'#clients .slider__next',
+	//	prev:'#clients .slider__prev',
+	//	circular:true
+	//});
 	$('#progects .slider__holder').scrollable({
 		next:'#progects .slider__next',
 		prev:'#progects .slider__prev',
 		circular:true
 	});
+
+
+	$('#clients .slider__slides').carouFredSel({
+		width: '100%',
+		auto: false,
+		prev: '#clients .slider__prev',
+		next: '#clients .slider__next'
+	});
+
 });
